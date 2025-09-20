423214dfec27e837f6831d34d4952379b53bdade
kolik29 <30481034+kolik29@users.noreply.github.com>
2025-09-20 14:55:55 +0300

    update Text

--- src/comonents/Inputs/Text/Text.tsx
+++ src/comonents/Inputs/Text/Text.tsx
@@ -2,12 +2,12 @@ import type { TextProps } from '../../../types/TextProps.types';
 import styles from './Text.module.sass';
 import cn from 'classnames';
 
-export function Text({ placeholder = '', label = '', children }: TextProps) {
+export function Text({ placeholder = '', label = '', name, value, children }: TextProps) {
   return (
     <div className={cn(styles.text)}>
       {label && <label className={styles.label}>{label}</label>}
       <div className={cn(styles['text-inner'])}>
-        <input type="text" placeholder={placeholder} />
+        <input type="text" placeholder={placeholder} name={name} value={value} />
         {children}
       </div>
     </div>
